0dcee17b316d588f22167b7aaea479f8b85428df
Octozek <octozek@gmail.com>
2024-06-30 21:13:14 -0500

    Update auth.js and replace bcrypt with bcryptjs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.JWT_SECRET || 'mysecretsshhhhh';
+const expiration = process.env.JWT_EXPIRES_IN || '2h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
