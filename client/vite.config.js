d6a7dda22902825800a0a69209ce29ccb2a485f0
Octozek <octozek@gmail.com>
2024-07-05 11:51:47 -0500

    made oen server instead of 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,11 +10,11 @@ export default defineConfig({
       '/graphql': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false,
-      },
-    },
+        secure: false
+      }
+    }
   },
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 });
